62a660bfa1523210942b3ea55b44f0731f7a4181
GABRIEL LAZARO BARBOSA <gabriel.lazaro@portalsesisp.org.br>
2026-08-11 10:11:56 -0300

    resolvendo erro página branca

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
+import { StoreProvider } from './store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </HashRouter>
   </React.StrictMode>,
 )
